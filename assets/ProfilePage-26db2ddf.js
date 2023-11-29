5690b5d476b955ab735250188419efab47c42bdf
andrii-kruk <119102553+andrii-kruk@users.noreply.github.com>
2023-11-29 15:28:26 +0000

    Deploying to gh-pages from @ andrii-kruk/message-app@20fc0f70d24ed71a3ff28eb4627bc4182bc73a66 🚀

---
+++ assets/ProfilePage-26db2ddf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-dd92cbcd.js";const t=()=>e.jsx("section",{children:"Content on profile page."});export{t as default};
--- assets/ProfilePage-383f240f.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-72051c20.js";const t=()=>e.jsx("section",{children:"Content on profile page."});export{t as default};
